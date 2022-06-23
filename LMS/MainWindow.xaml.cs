8804e2adf68a1d60196646be1d726f2c7ad672be
Wojciech Wolski <wojciech.wolski@microsoft.wsei.edu.pl>
2022-06-23 12:23:55 +0200

    login window - drag, exit, signupbtn, loginbtn, connect to base

--- LMS/MainWindow.xaml.cs
+++ LMS/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace LMS
 {
@@ -24,5 +26,45 @@ namespace LMS
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "data source = localhost ; database=library; integrated security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select * from Admins where username ='" + tboxUsername.Text + "' and password ='" + boxPassword.Password + "'";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                this.Hide();
+               // Panel pnl = new Panel();
+               // pnl.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong username/password!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Signup sgn = new Signup();
+            sgn.Show();
+        }
+
     }
 }
