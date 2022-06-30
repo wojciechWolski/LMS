14edce5724b0ffea84c22b90a8ff5f28568e2885
Wojciech Wolski <wojciech.wolski@microsoft.wsei.edu.pl>
2022-06-30 20:57:02 +0200

    X button

---
+++ LMS/ReturnViewWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LMS
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ReturnViewWindow.xaml
+    /// </summary>
+    public partial class ReturnViewWindow : Window
+    {
+        public ReturnViewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
